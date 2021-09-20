bdc58e435dcae0ee901107bd055a3a99bd661ca0
nataliaalvs <natalia.reges6@gmail.com>
2021-09-20 19:09:26 -0300

    nat

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página inicial</title>
+    <title>New</title>
 </head>
 <body>
+    <h1>Adicione um curso novo</h1>
     <div class="container">
-        <h1>Seja bem vindo a spitech!</h1>
+        <label for="nome">Qual o nome do seu curso?</label>
+        <input type="text" name="nome" id="nome">
     </div>
 </body>
 </html>
