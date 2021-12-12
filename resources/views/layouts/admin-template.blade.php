f3b0d075ad7ee533c677077f6590e9a1a71ce5fe
nataliaalvs <natalia.reges6@gmail.com>
2021-12-12 14:39:55 -0300

    logout

--- resources/views/layouts/admin-template.blade.php
+++ resources/views/layouts/admin-template.blade.php
@@ -34,7 +34,10 @@
                         {{-- <li><a class="dropdown-item" href="#!">Settings</a></li>
                         <li><a class="dropdown-item" href="#!">Activity Log</a></li>
                         <li><hr class="dropdown-divider" /></li> --}}
-                        <li><a class="dropdown-item" href="#!">Logout</a></li>
+                        <form id="logout" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                         </form>
+                        <li><a class="dropdown-item" onclick="document.getElementById('logout').submit(); return false;">Logout</a></li>
                     </ul>
                 </li>
             </ul>
